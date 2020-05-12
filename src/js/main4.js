0208b26b47fdd18b806cb11ef1b8fe2003f29bae
yj <apple@AppletekiMacBook-Air-4.local>
2020-05-12 14:04:25 +0800

    r

--- src/js/main4.js
+++ src/js/main4.js
@@ -1,5 +1,10 @@
 
 
+
+import {RxmActionSheet, RxmAppTool }from  '@carchy/jtlib'
 document.write('<h2>bundle4</h2>');
 
 export default ()=>{document.write('<h2>bundle4</h2>')}
+let sss =new RxmActionSheet()
+/**@type  {import '@carchy/jtlib'.RxmAppTool}*/
+let  app
